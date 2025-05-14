fd180e56ac065687bd09e7bddef3f6c0dca464a2
Alvarciu <alvaroer2004@gmail.com>
2025-05-14 12:39:22 +0200

    Logout completado

--- relecloudGrC08/project/settings.py
+++ relecloudGrC08/project/settings.py
@@ -199,3 +199,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'berural.app@gmail.com'
 EMAIL_HOST_PASSWORD = 'bicxhggdyjhqkxkw'
+
+LOGOUT_REDIRECT_URL = '/'
+
--- relecloudGrC08/relecloud/urls.py
+++ relecloudGrC08/relecloud/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -51,5 +53,7 @@ urlpatterns = [
     # Verificar correos duplicados
     path("verificar-email/", views.verificar_email, name="verificar_email"),
 
+    # Para hacer logout del perfil
+    path("logout/", LogoutView.as_view(next_page='index'), name="logout"),
 
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
