8b32d97d42c2b35d56f93ec5670c19a8e9591b84
Rodrigo Justo Perez <rodrigojustoperez@gmail.com>
2025-05-14 19:35:50 +0200

    Cambios settings

--- relecloudGrC08/project/settings.py
+++ relecloudGrC08/project/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 AUTH_USER_MODEL = 'relecloud.Usuario'  # Ya que usas un modelo de usuario personalizado
 
