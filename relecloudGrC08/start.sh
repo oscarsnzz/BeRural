58bf17baedc5629aeacd4586f7ab773c7b376110
Rodrigo Justo Perez <rodrigojustoperez@gmail.com>
2025-05-14 20:00:19 +0200

    Usar start.sh para iniciar daphne en Render1

--- relecloudGrC08/start.sh
+++ relecloudGrC08/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Starting server on port $PORT..."
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 exec daphne -b 0.0.0.0 -p "$PORT" project.asgi:application
